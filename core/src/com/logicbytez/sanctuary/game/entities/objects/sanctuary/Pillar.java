92da7191921b7cb443f2eafed5d72635574da95b
ntctk4 <ntctk4@mail.missouri.edu>
2017-02-21 12:46:36 -0600

    Started on the pillar class for the magical pillars

--- core/src/com/logicbytez/sanctuary/game/entities/objects/sanctuary/Pillar.java
+++ core/src/com/logicbytez/sanctuary/game/entities/objects/sanctuary/Pillar.java
@@ -1,11 +1,32 @@
 package com.logicbytez.sanctuary.game.entities.objects.sanctuary;
 import com.badlogic.gdx.maps.MapObject;
+import com.logicbytez.sanctuary.Assets;
 import com.logicbytez.sanctuary.game.GameScreen;
 import com.logicbytez.sanctuary.game.entities.Entity;
 
 public class Pillar extends Entity{
+	
+	int stonesInserted;
+	MapObject object;
+	//can keep strack 
 	public Pillar(GameScreen game, MapObject object){
 		super(game, object);
+		this.object = object;
+		
+		impede = true;
+		stonesInserted = 0;
 		//This needs to be implemented!
 	}
+	
+	public void insertStone(){
+		
+		if(stonesInserted < 4 && game.getHud().getStones() > 0)
+		{
+			Assets.sound_Sunstone.play();
+			game.getHud().addStone(false);
+			sprite.setRegion(animation.getKeyFrame(stonesInserted));
+		}
+	
+	}
+
 }
