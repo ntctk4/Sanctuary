a2eebeb480b169760a19f910af8324eeb3beae51
ntctk4 <ntctk4@mail.missouri.edu>
2017-03-07 13:46:15 -0600

    changed the collision box so sun stones can be inserted on all four sides of the pillar

--- core/src/com/logicbytez/sanctuary/game/entities/objects/sanctuary/Pillar.java
+++ core/src/com/logicbytez/sanctuary/game/entities/objects/sanctuary/Pillar.java
@@ -1,6 +1,7 @@
 package com.logicbytez.sanctuary.game.entities.objects.sanctuary;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.math.Rectangle;
 import com.logicbytez.sanctuary.Assets;
 import com.logicbytez.sanctuary.game.GameScreen;
 import com.logicbytez.sanctuary.game.entities.Entity;
@@ -9,17 +10,16 @@ public class Pillar extends Entity{
 	
 	int stonesInserted;
 	MapObject object;
-	//can keep strack 
 	public Pillar(GameScreen game, MapObject object){
 		super(game, object);
 		this.object = object;
 		animation = Assets.animate(1, 1, 0, Assets.texture_Pillar);
-		//animation = Assets.animate(11, 1, 0, Assets.texture_Piller);
 		impede = true;
+		collisionBox = new Rectangle(box.x, box.y, box.width - 2, box.height);
+		collisionBox.fitInside(box);
 		stonesInserted = 0;
 		sprite = new Sprite(animation.getKeyFrame(stonesInserted));
 		sprite.setPosition(box.x, box.y);
-		//This needs to be implemented!
 	}
 	
 	public void insertStone(){
